81ffbd4dd6993621332aa8e3a20105dd1074f82b
srexrg <sreeragp276@gmail.com>
2024-05-01 19:50:50 +0530

    cors config

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ dotenv.config({
 const app = express();
 
 app.use(cors({
-  origin:["https://nourish-net-backend.vercel.app"],
+  origin:["https://nourish-net-henna.vercel.app"],
   methods:["GET","POST","DELETE","PATCH"],
   credentials:true
 }));
